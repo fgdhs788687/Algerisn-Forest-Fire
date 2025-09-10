761d87fa4e09dad580c5a8f3f56cc091c13dbf5e
Mohammed Faizal Razza <faizalrazza756@gmail.com>
2025-09-10 14:39:25 +0530

    first commit

---
+++ applicationn.py
@@ -0,0 +1,46 @@
+import pickle
+from flask import Flask,request,jsonify,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+application = Flask(__name__)
+app = application
+
+# Import ridge regressor and standard scaler pickle
+ridge_model = pickle.load(open('models/ridge_regression.pkl','rb'))
+standard_scaler = pickle.load(open('models/scaler.pkl','rb'))
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/fwi_prediction")
+def fwi_prediction():
+    return render_template('advance_home.html')
+
+@app.route("/predictdata",methods=['GET','POST'])
+def predict_datapoint():
+    if request.method == 'POST':
+        Temperature = float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
+        # Using Standard Scaler to scale the data:
+        new_dat_scaled = standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+
+        # Using Ridge Regression model to make prediction:
+        result = ridge_model.predict(new_dat_scaled)
+
+        return render_template('advance_home.html', results=result[0])
+    else:
+        return render_template('advance_home.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0") # This basically mapped to the local IP address of any machine that you are working
